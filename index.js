fa46da62e06e61a6bfb9501ece7e592fac912a40
israelitalo <israelitalo2012@gmail.com>
2021-01-26 22:21:17 -0300

    Atualização de lib

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 var bodyParser = require('body-parser')
 var express = require("express")
+const cors = require('cors')
 var app = express()
 var router = require("./routes/routes")
 
+app.use(cors());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
